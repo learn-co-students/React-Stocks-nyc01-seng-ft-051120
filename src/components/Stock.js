a4d6a102157553a8cb7424f3447164d4dbc002db
Gary Cordero <GaryCordero1690@gmail.com>
2020-07-17 15:34:58 -0400

    completed

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,20 +1,17 @@
 import React from 'react'
 
-const Stock = () => (
+const Stock = (props) => (
   <div>
-
-    <div className="card">
+    <div className="card" onClick={()=>props.handleClick(props.id)}>
       <div className="card-body">
         <h5 className="card-title">{
-            //Company Name
+            props.name
           }</h5>
         <p className="card-text">{
-            //ticker: stock price
+            `${props.ticker} : ${props.price}`
           }</p>
       </div>
     </div>
-
-
   </div>
 );
 
