cd9616a60c768e83373f7cbaa44e24d89e18446f
steveneross94 <ross.steven@me.com>
2020-07-18 11:09:50 -0400

    Done.

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -5,21 +5,82 @@ import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
 
+  componentDidMount(){ //When the Dom Loads 
+    fetch('http://localhost:3000/stocks') //fetch stocks
+    .then(r => r.json()) 
+    .then(stocks => {
+      this.setState({ // change the state 
+        stocks: stocks //equal to our fetch 
+      })
+    })
+  }
+  
+  state = {
+    stocks: [],
+    myStocks: [],
+    filter: 'All',
+    sort: 'None'
+  }
+
+  buyStock = (id) => {
+    if (!this.state.myStocks.find(pId => pId === id)){
+      this.setState({
+        myStocks: [...this.state.myStocks, id]
+      })
+      console.log(this.state);
+    }
+    }
+
+    removeStock = (id) => {
+      let newStocks = this.state.myStocks.filter(pId => pId !== id)
+        this.setState({
+          myStocks: newStocks
+        })
+      console.log(this.state)
+    }
+
+    updateSort = sortBy => {
+      this.setState({ sort: sortBy })
+    }
+
+    updateFilter = type  => {
+      this.setState({ filter: type })
+    }
+
+
+   calculateDisplayStocks = () => {
+    let filteredStocks = [...this.state.stocks]
+    if (this.state.filter !== 'All') {
+        filteredStocks = filteredStocks.filter(stock => stock.type === this.state.filter)
+    }
+
+    if (this.state.sort === 'Alphabetically'){
+      return filteredStocks.sort((a,b) => a.name > b.name ? 1 : -1)
+    } else if (this.state.sort === 'Price'){
+      return filteredStocks.sort((a,b) => a.price > b.price ? 1 : -1 )
+    } else {
+      return filteredStocks
+    }
+   }
+   
   render() {
+    let myPortfolio = this.state.myStocks.map(id => this.state.stocks.find(stock => stock.id === id))
+    let displayStocks = this.calculateDisplayStocks()
     return (
       <div>
-        <SearchBar/>
-
+        <SearchBar filter={this.state.filter} sort={this.state.sort} updateFilter={this.updateFilter} updateSort={this.updateSort}/>
           <div className="row">
             <div className="col-8">
-
-              <StockContainer/>
-
+              <StockContainer 
+                buyStock={this.buyStock}
+                stocks={displayStocks}
+              />
             </div>
             <div className="col-4">
-
-              <PortfolioContainer/>
-
+              <PortfolioContainer 
+                stocks={myPortfolio}
+                removeStock={this.removeStock}
+              />
             </div>
           </div>
       </div>
