9696b9a5d913d8ba2d9a409fe350275fbb545aaf
chloekim917 <chloekim917@gmail.com>
2020-07-21 22:09:54 -0400

    Done.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,23 @@
 import React from 'react';
 
-const SearchBar = () => {
+const SearchBar = (props) => {
   return (
     <div>
 
       <strong>Sort by:</strong>
       <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
+        <input type="radio" value="Alphabetically" checked={props.sortBy === 'Alphabetically'} onChange={props.handleSort}/>
         Alphabetically
       </label>
       <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
+        <input type="radio" value="Price" checked={props.sortBy === 'Price'} onChange={props.handleSort}/>
         Price
       </label>
       <br/>
 
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select onChange={props.onTypeChange}>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -2,18 +2,13 @@ import React, { Component } from 'react';
 import Stock from '../components/Stock'
 
 class StockContainer extends Component {
-
   render() {
     return (
       <div>
         <h2>Stocks</h2>
-        {
-          //render the list of stocks here
-        }
+        {this.props.stocks.map(stock => 
+        <Stock key={stock.id} {...stock} stockAction={this.props.handleBuy}/>)}
       </div>
-    );
-  }
-
+  )}
 }
-
 export default StockContainer;
